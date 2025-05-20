544a378aeffb9af383374822f17d18f0e4b131b7
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-20 13:44:19 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int add(){
+    int x,y,sum=0;
+    scanf("%d%d",&x,&y);
+    sum=x+y;
+    return sum;
+}
+int main()
+{
+  int x= add();
+   printf("%d",x);
+}
